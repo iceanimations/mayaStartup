d6b9a8609d377ab0afc6199aab02049a6729732c
Talha Ahmed <talha.ahmed@gmail.com>
2016-10-20 11:48:52 +0500

    clean up

--- src/jobs.py
+++ src/jobs.py
@@ -6,19 +6,12 @@ Created on Jul 6, 2015
 import pymel.core as pc
 
 from uiContainer import uic
-from PyQt4.QtGui import QMessageBox, QFileDialog, qApp, QPushButton, QRadioButton, QDialog
-import os
-import re
 import cui
 reload(cui)
 import os.path as osp
 import qtify_maya_window as qtfy
-import pymel.core as pc
-import appUsageApp
-import msgBox
 import qutil
 reload(qutil)
-import re
 import maya.cmds as cmds
 
 root_path = osp.dirname(osp.dirname(__file__))
@@ -29,25 +22,25 @@ class FPSDialog(Form, Base):
     def __init__(self, parent=qtfy.getMayaWindow()):
         super(FPSDialog, self).__init__(parent)
         self.setupUi(self)
-        
+
         for key in qutil.FPS_MAPPINGS:
             self.fpsBox.addItem(key)
-            
+
         # set the current fps
         fps = self.getCurrentFPS()
         for i in range(self.fpsBox.count()):
             if fps in self.fpsBox.itemText(i):
                 self.fpsBox.setCurrentIndex(i)
                 break
-        
+
         self.okButton.clicked.connect(self.ok)
-        
+
     def getCurrentFPS(self):
         return pc.currentUnit(time=True, q=True)
-    
+
     def getSelectedFPS(self):
         return self.fpsBox.currentText()
-    
+
     def ok(self):
         fps = self.getSelectedFPS()
         for key in qutil.FPS_MAPPINGS:
